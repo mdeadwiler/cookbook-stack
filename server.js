54744fb9d5d49e4e3f225514472fa90f36cc3c77
MDeadwiler <mdeadwiler@email.com>
2024-10-02 10:27:08 -0400

    stack-cookbook

--- server.js
+++ server.js
@@ -6,8 +6,14 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
-
+const router = express.Router();
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 const authController = require('./controllers/auth.js');
+
+const recipesController = require('./controllers/recipes.js');
+const ingredientsController = require('./controllers/ingredients.js');
+
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -41,8 +47,10 @@ app.get('/vip-lounge', (req, res) => {
     res.send('Sorry, no guests allowed.');
   }
 });
-
+app.use(passUserToView);
 app.use('/auth', authController);
+app.use('/recipes', recipesController);
+app.use('/ingredients', ingredientsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);

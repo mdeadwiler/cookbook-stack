f3496047aa1d673e753650a238b8bac4671031e6
MDeadwiler <mdeadwiler@email.com>
2024-10-02 14:12:20 -0400

    stack-cookbook

--- models/ingredients.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ingredientSchema = new mongoose.Schema({
-name: String,
-});
-
-const Ingredient = mongoose.model('Ingredient', ingredientSchema);
-
-module.exports = Ingredient;
